86a1cb9f15627288b925ce2db3626962fe32ae0f
AmalH <hichri.amal@hotmail.fr>
2018-05-11 16:29:19 +0100

    final

--- example 3/server.js
+++ example 3/server.js
@@ -37,7 +37,7 @@ int3 = setInterval(jeu, 10);
 intseuil = setInterval(setseuil,100)
 
 
-
+//----------------------------------------- LED ---------------------------------
   app.get('/ledon',function(request, response) {
    response.json("on");
    myLcd.clear();
@@ -45,7 +45,7 @@ intseuil = setInterval(setseuil,100)
   myLcd.setColor(0, 255, 0);
   myLcd.write('on');  
   isCardOn = true
-
+  
   int1 = setInterval(readLightSensorValue, 100);
   int2 = setInterval(readTempSensorValue, 100);
   int3 = setInterval(jeu, 10);
@@ -60,69 +60,57 @@ app.get('/ledoff',function(request, response) {
 myLcd.setColor(255, 0 , 0);
 myLcd.write('off');  
 isCardOn = false
-
 clearInterval(int1)
 clearInterval(int2)
 clearInterval(int3)
 clearInterval(intseuil)
 });
 
+//----------------------------------------- Light ---------------------------------
+ function readLightSensorValue() {
+    console.log("light value:"+light.value());
+    app.get('/getLum',function(request, response) {
+        response.json({value:light.value(),etat:isCardOn?"on":"off",ip:"192.168.10.81"});
+    });
+    if(light.value()>seuillum && !isalertlum){
+        isalertlum = true
+        request.get(urlsetAlert+"?ip=192.168.10.81&valeur="+light.value()+"&capteur=Luminosite",function (error, responce, body){
+            if(!error && responce.statusCode == 200 )
+                {console.log(body)
+                }
+            });
+            board.digitalWrite(13, (1));
+    }
+    else if(light.value()<=seuillum) {board.digitalWrite(13, (0)); isalertlum = false}
+}
+//----------------------------------------- Temperature ---------------------------------
+function readTempSensorValue() {
+    console.log("temp value:"+temp.value());
+    app.get('/getTemp',function(request, response) {
+        response.json({value:temp.value(),etat:isCardOn?"on":"off",ip:"192.168.10.81"});
+    });
+    if(temp.value()>seuil && !isalerttemp){
+        isalerttemp = true
+        request.get(urlsetAlert+"?ip=192.168.10.81&valeur="+temp.value()+"&capteur=Temperature",function (error, responce, body){
+            if(!error && responce.statusCode == 200 )
+                {console.log(body)
+                }
+            });
+            board.digitalWrite(8, (1));
+    }
+    else if(temp.value()<=seuil) {board.digitalWrite(8, (0)); isalerttemp = false} 
+}
 
-app.get('/getEtat',function(request, response) {
-    response.json(isCardOn?"on":"off");
- });
+//----------------------------------------- Services ---------------------------------
+function testAlert() {
+    request.get("http://192.168.236.1:4300/setAlertTemp",function (error, responce, body){
+        if(!error && responce.statusCode == 200 )
+            {console.log(body)
+            }
+        });
+ }
  
-    function readLightSensorValue() {
-        console.log("light value:"+light.value());
-        app.get('/getLum',function(request, response) {
-            response.json({value:light.value(),etat:isCardOn?"on":"off",ip:"192.168.10.81"});
-        });
-        if(light.value()>seuillum && !isalertlum){
-            isalertlum = true
-            request.get(urlsetAlert+"?ip=192.168.10.81&valeur="+light.value()+"&capteur=Luminosite",function (error, responce, body){
-                if(!error && responce.statusCode == 200 )
-                    {console.log(body)
-                    }
-                });
-                board.digitalWrite(13, (1));
-        }
-        else if(light.value()<=seuillum) {board.digitalWrite(13, (0)); isalertlum = false}
-    }
-
-    function readTempSensorValue() {
-        console.log("temp value:"+temp.value());
-        app.get('/getTemp',function(request, response) {
-            response.json({value:temp.value(),etat:isCardOn?"on":"off",ip:"192.168.10.81"});
-        });
-        if(temp.value()>seuil && !isalerttemp){
-            isalerttemp = true
-            request.get(urlsetAlert+"?ip=192.168.10.81&valeur="+temp.value()+"&capteur=Temperature",function (error, responce, body){
-                if(!error && responce.statusCode == 200 )
-                    {console.log(body)
-                    }
-                });
-                board.digitalWrite(8, (1));
-        }
-        else if(temp.value()<=seuil) {board.digitalWrite(8, (0)); isalerttemp = false} 
-    }
-
-    //© bitsplz
-    var x = 1
-    var x1 =100
-    var x2 = 200
-    function jeu(){
-        myLcd.setColor(x,x1,x2);
-
-        x=x+6
-        x1=x1+15
-        x2=x2+10
-
-        if (x>255) x= 1
-        if (x1>255) x1= 1
-        if (x2>255) x2= 1
-    }
-
-    app.get('/valuelcd/:value', function (request, response) {
+ app.get('/valuelcd/:value', function (request, response) {
         myLcd.clear();
         myLcd.setCursor(0, 0);
         myLcd.setColor(255, 255, 0);
@@ -142,15 +130,32 @@ app.get('/getEtat',function(request, response) {
         console.log("seuil : "+seuil)   
     }
 
-    /*setInterval(function() {  
-        if (buttonstop.value()) board.digitalWrite(8, (0));
-    });*/
+    app.get('/getEtat',function(request, response) {
+        response.json(isCardOn?"on":"off");
+     });
 
     setInterval(function() {  
         app.get('/info',function(request, response) {
             response.json({temperature:temp.value(),luminosite:light.value(),etat:isCardOn,ip:"192.168.10.81",name:"bitsplz"});
         });
     },50);
+
+ /* bitsplz
+    var x = 1
+    var x1 =100
+    var x2 = 200
+
+    function jeu(){
+        myLcd.setColor(x,x1,x2);
+
+        x=x+6
+        x1=x1+15
+        x2=x2+10
+
+        if (x>255) x= 1
+        if (x1>255) x1= 1
+        if (x2>255) x2= 1
+    }*/
 
 
 app.listen(app.get('port'), function () {
